a47e435a5b4c4413e1a7625187a4e4766ce0cbbb
dikshant2005 <dikshantk809@gmail.com>
2024-08-06 22:59:03 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,82 @@
+# print("hello world") 
+# print(213356) # show number 
+# print (dikshant)
+# '''this
+# is
+# multi
+# line'''
+# # variables :
+# a = 10 # integer --> int
+# b =23.4 # decimal --> float
+# c = 'hello' # string --> str 
+# print (c)
+# print(b)
+# print(c)
+# print ('c')
+# print (type(a))
+# print (type (b))
+# print(type (c))
+# rules to give variable name :
+# space , special symbol is not allowed 
+# you can give alphabhets (A-Z,a-z),number  (0-9) and underscrore (_)
+# number at first prosition in not allowed
+# first_name = 'dikshant' #snake casing
+# FirstName = "dikshnt" #pascal casing 
+# firstName = "dikshant" # camel casing
+# a = 10
+# a=20
+# print(a)
+# # case senstivity :
+# a =10
+# A = 20
+# print(a)
+# indentation --> spacing
+# you shoukd be carefull while giving indent at begining of line 
+# a = 10
+# b= 20
+# print (a*b)
+# print (a + b)
+# print (a-b )
+# assiniging multiple variables :
+# a,b,c = 10 ,'hello', 33.3
+# print (a)
+# print (b)
+# print (c)
+# print (a,b,c)
+# a=b=c=10
+# print(a,b,c)
+# print('the value  of a is ',a)
+# print('hello worls',end="")
+# print('hello worls',end='----')
+# print('hello worls',end="dikshnt")
+# print('hello worls')
+# ---------------------------------------------------------------------
+# type   casting :
+# type casting is used to change data type of a variable 
+# a = 10
+b= 10.5
+c = 'hello'
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# c = int (c)
+print(type(b))
+
+# a = float (a)
+# print (a)
+
+# b= str(b)
+# print(type(b))
+
+ 
+# swaping two numbers :
+# a=10
+# b=20
+# temp=a
+# a=b
+# temp=b
+# b=a
+# a,b=b,a
+# print(b)
+
+
